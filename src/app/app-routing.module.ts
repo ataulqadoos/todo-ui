ac775aaa2aae0ae0461a79d8c2a618befd384c05
ataulqadoos <ataulqadoos@gmail.com>
2020-01-11 22:33:10 -0500

    Added detail component and link on list component to detail.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {TodoItemDetailComponent} from "./todo-item-detail/todo-item-detail.component";
 
 
 const routes: Routes = [
-  // { path: 'list', component: PersonListComponent},
-  // { path: 'search', component: PersonSearchComponent},
+  { path: 'list', component: TodoListComponent},
+  { path: 'detail/:id', component: TodoItemDetailComponent},
   { path: '', redirectTo: '/list', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import {TodoService} from "./service/todo.service";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
+import { TodoItemDetailComponent } from './todo-item-detail/todo-item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoItemDetailComponent
   ],
   imports: [
     BrowserModule,
