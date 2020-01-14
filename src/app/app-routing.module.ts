fd523634ff154ada7ba5d48fa494070ece77602e
ataulqadoos <ataulqadoos@gmail.com>
2020-01-13 23:25:02 -0500

    Updated service with crud operations. Added forms for detail and edit todo item.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {TodoListComponent} from "./todo-list/todo-list.component";
 import {TodoItemDetailComponent} from "./todo-item-detail/todo-item-detail.component";
+import {TodoItemEditComponent} from "./todo-item-edit/todo-item-edit.component";
 
 
 const routes: Routes = [
   { path: 'list', component: TodoListComponent},
+  { path: 'create', component: TodoItemEditComponent},
   { path: 'detail/:id', component: TodoItemDetailComponent},
   { path: '', redirectTo: '/list', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,34 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import { TodoItemDetailComponent } from './todo-item-detail/todo-item-detail.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {ReactiveFormsModule} from "@angular/forms";
+import { TodoItemEditComponent } from './todo-item-edit/todo-item-edit.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    TodoItemDetailComponent
+    TodoItemDetailComponent,
+    TodoItemEditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule,
     MatTableModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     BrowserAnimationsModule
   ],
   providers: [TodoService],
